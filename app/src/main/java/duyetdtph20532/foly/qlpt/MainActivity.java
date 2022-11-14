06cefb66d11d339421a58b5d70475f6aa6475510
Duyetdtph20532 <117613152+Duyetdtph20532@users.noreply.github.com>
2022-11-14 07:53:01 +0700

    demo 1

---
+++ app/src/main/java/duyetdtph20532/foly/qlpt/MainActivity.java
@@ -0,0 +1,14 @@
+package duyetdtph20532.foly.qlpt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
